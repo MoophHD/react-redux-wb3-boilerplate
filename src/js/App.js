e5e876431761e1f36d2eac2a9fa95c30c5b85671
MoophHD <d_sizykh@mail.ru>
2018-01-11 17:08:55 +0300

    :tada:

--- src/js/App.js
+++ src/js/App.js
@@ -5,24 +5,13 @@ import store from './store';
 
 import '../scss/app';
 
-import Primary from './partials/navigation/primary';
-import Home from './views/Home';
-import About from './views/About';
-import NotFound from './views/NotFound';
 
 export default class App extends Component {
-  
   render() {
     return (
       <Provider store={store}>
         <Router>
           <div>
-            <Primary />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route component={NotFound}/>
-            </Switch>
           </div>
         </Router>
       </Provider>
@@ -30,3 +19,10 @@ export default class App extends Component {
   }
   
 }
+
+    // <Primary />
+    // <Switch>
+    //   <Route exact path="/" component={Home} />
+    //   <Route path="/about" component={About} />
+    //   <Route component={NotFound}/>
+    // </Switch>
